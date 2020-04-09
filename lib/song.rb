5a70d76d43dcf9ed5d6bde0e41da9f8dd2c40133
chris brady <chrisb3546@yahoo.com>
2020-04-09 18:08:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   
   attr_accessor :name, :artist, :genre
 
-Vdef initialize(name, artist, genre)
+def initialize(name, artist, genre)
  # @@count +=1
   #@@genres =[]
   #@@artists = []
