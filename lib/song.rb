038131db4756edcdfe5b48dbbea24aed701d8a51
chris brady <chrisb3546@yahoo.com>
2020-04-09 18:19:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,14 @@ class Song
   
   attr_accessor :name, :artist, :genre
 @@count = 0
+
 @@artists = []
+
 @@genres = []
 def initialize(name, artist, genre)
   @@count +=1
   @@genres =[]
-  @@artists = []
+  @@artists = @@artists =[]
   @name= name
   @artist = artist
   @genre= genre
