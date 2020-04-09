94de6c6ccbb74732c92f1eb998cf9c0604b94057
chris brady <chrisb3546@yahoo.com>
2020-04-09 17:04:21 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song 
+  
+  attr_accessor :name, :genre, :song
+
+@@count = 0
+@@genres = []
+@@artists = []
+
+def initialize(name, genre, song)
+  @@count +=1
+  @@genres =[]
+  @@artists = []
+end 
+
+def self.count
+end 
+  
+def self.genre 
+end
+
+def self.artists
+end 
+
+def self.genre_count
+end
+
+def self.artist_count
+end 
+
+end
+
+hit_me = Song.new
+hit_me.name = ("hit me baby one more time")
